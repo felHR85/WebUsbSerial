90d5175288ad61ee3fb893c9b6c8aad906cf983e
Felipe Herranz <felhr85@gmail.com>
2019-03-19 00:12:48 +0100

    added functions for finding CP210x devices

--- src/usbserial/listDevices.js
+++ src/usbserial/listDevices.js
@@ -1,29 +1,61 @@
+import{cp210xIds} from '../vidpid/cp210xids.js'
+
 export async function getAllUsbDevices() {
     let devices = await navigator.usb.getDevices();
     return devices;
 }
 
-export async function getAvailableDevices(callback) {
-    let devices = await navigator.usb.getDevices();
-    //TODO: Compare VID/PIDS 
+export async function getAvailableDevices() {
+    let availableDevices = await getCP201xDevices();
+    if(availableDevices.length > 0) {
+        return availableDevices;
+    }
+
+    let availableDevices = await getFTDIDevices();
+    if(availableDevices.length > 0) {
+        return availableDevices;
+    }
+
+    let availableDevices = await getPL2303Devices();
+    if(availableDevices.length > 0) {
+        return availableDevices;
+    }
+
+    let availableDevices = await getCDCDevices();
+    if(availableDevices.length > 0) {
+        return availableDevices;
+    }
+
+    let availableDevices = await getCh34xDevices();
+    if(availableDevices.length > 0) {
+        return availableDevices;
+    }
 }
 
 export async function getCP201xDevices() {
-    //TODO!!
+    let devices = await navigator.usb.getDevices();
+    return devices.filter(function(item) {
+        return cp210xIds.values.find(ids => 
+            (ids.vid == item.vendorId && ids.pid == item.productId)).length == 1;
+    });
 }
 
 export async function getFTDIDevices() {
     //TODO!!
+    return [];
 }
 
 export async function getPL2303Devices() {
     //TODO!!
+    return [];
 }
 
 export async function getCDCDevices() {
     //TODO!!
+    return [];
 }
 
 export async function getCh34xDevices() {
     //TODO!!
+    return [];
 }
